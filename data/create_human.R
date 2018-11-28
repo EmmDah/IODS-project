3c9e5a3028ad90a63529ea48cb6953dbd96656df
EmmDah <44607944+EmmDah@users.noreply.github.com>
2018-11-28 22:50:22 +0200

    exercise 5 added
    updated also headers in exercise 4

--- data/create_human.R
+++ data/create_human.R
@@ -54,4 +54,83 @@ dim(human);str(human)
 #save data
 write.table(human, "~/GitHub/IODS-project/data/human_2018.txt", quote=F, sep="\t", row.names=F)
 
+#Exercise 5
 
+#read in data:
+
+human <- read.delim("~/GitHub/IODS-project/data/human_2018.txt")
+str(human)
+head(human)
+
+#rename according file in datacamp exercise
+
+
+?rename()
+
+library(stringr)
+
+human <- rename(human,
+                GNI = GNI_per_Capita,
+                Life.Exp = Life_expectancy,
+                Edu.Exp = Exp_education,
+                Edu.Mean =Mean_education,
+                GII.Rank=GII_rank,
+                GII=GII_index,
+                Mat.Mor =MMR,
+                Ado.Birth=ABR, 
+                Parli.F=PR,
+                Edu2.F=SE_females,
+                Edu2.M=SE_males,
+                Labo.F=LFPR_females,
+                Labo.M=LFPR_males,
+                Edu2.FM=SE_ratio_FM, 
+                Labo.FM=LFPR_ratio_FM)
+                
+head(human)
+#GNI colum values are not okay, look at its structure
+str(human$GNI)
+  #Factor w/ 194 levels "1,096","1,123",..: 166 135 156 139 140 137 127 154 134 117
+
+# remove the commas from GNI and print out a numeric version of it
+library(tidyr);library(stringr)
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric 
+?mutate()
+head(human);str(human$GNI)
+#looks good!
+
+#select a subset of the variables in human and drop the others from the dataset
+keep <-  c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp",
+           "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+human <- select(human, one_of(keep))
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+#human_ <- filter(human, complete.cases(human))
+human_ <- human[complete.cases(human), ]
+
+
+# look at the last 10 observations
+tail(human_, 10)
+
+# last indice we want to keep
+last <- nrow(human_) - 7
+
+# choose everything until the last 7 observations
+human_ <- human[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$Country
+
+#remove country column
+human_ <- dplyr::select(human_, -Country) 
+
+str(human_);head(human_)
+
+
+write.table(human_, "~/GitHub/IODS-project/data/human_new_2018.txt", quote=F, sep="\t", row.names=T)
